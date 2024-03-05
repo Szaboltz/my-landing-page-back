108a27e51a78ea3440e2da5699cdb7ff8f3936d1
Henrique Szabo <szabopessoal@gmail.com>
2024-03-05 18:22:10 -0300

    Update: Clean controller and add Cross Origin

--- src/main/java/com/szabo/landingPagebackend/message/MessageController.java
+++ src/main/java/com/szabo/landingPagebackend/message/MessageController.java
@@ -1,22 +1,20 @@
 package com.szabo.landingPagebackend.message;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpRequest;
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/")
 public class MessageController {
 	private final MessageService messageService;
-	@Autowired
+
 	public MessageController(MessageService messageService){
 		this.messageService = messageService;
 	}
 
-	@GetMapping
-	public void getMessage(){}
+	// @GetMapping
+	// public void getMessage(){}
+
 	@PostMapping("create")
 	public void create(@RequestBody MessageModel messageModel){
 		messageService.validateMessage(messageModel);
--- src/main/java/com/szabo/landingPagebackend/message/MessageService.java
+++ src/main/java/com/szabo/landingPagebackend/message/MessageService.java
@@ -1,6 +1,5 @@
 package com.szabo.landingPagebackend.message;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -9,15 +8,14 @@ import java.time.format.DateTimeFormatter;
 @Service
 public class MessageService {
 	private final MessageRepository messageRepository;
-	@Autowired
+
 	public MessageService(MessageRepository messageRepository){
 		this.messageRepository = messageRepository;
 	}
 
 	private String dateToString(LocalDateTime data){
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-		String formattedDateTime = data.format(formatter);
-		return  formattedDateTime;
+		return data.format(formatter);
 	}
 
 	public void validateMessage(MessageModel messageModel) {
