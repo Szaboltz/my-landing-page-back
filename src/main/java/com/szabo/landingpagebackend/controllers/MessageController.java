d685d5236508d7dc41da8e9d7c9fb87047f47cde
Henrique Szabo <szabopessoal@gmail.com>
2024-03-28 10:52:00 -0300

    Update: Convert Json into a string

--- src/main/java/com/szabo/landingpagebackend/controllers/MessageController.java
+++ src/main/java/com/szabo/landingpagebackend/controllers/MessageController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.szabo.landingpagebackend.services.EmailService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
